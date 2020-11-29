25a641037a987d79565e2d76493db2a5049beba6
Jon Smith <ilovebball4life@gmail.com>
2020-11-28 21:53:59 -0600

    ExDialog process fix

--- ExtendedAPI/src/oxygen/api/game/dialog/ExDialog.java
+++ ExtendedAPI/src/oxygen/api/game/dialog/ExDialog.java
@@ -7,6 +7,8 @@ import oxygen.api.action.MenuAction;
 import oxygen.api.events.InterfaceInteractEvent;
 import oxygen.api.wrappers.InterfaceComponent;
 
+import java.util.Arrays;
+
 public class ExDialog {
 
     private static final int DIALOG_GROUP = 219;
@@ -15,11 +17,21 @@ public class ExDialog {
         return Interfaces.isOpen(DIALOG_GROUP);
     }
 
+    public static boolean isViewingChatOptions() {
+        return Interfaces.getComponent(DIALOG_GROUP).length > 0;
+    }
+
     public static boolean process(String... options) {
         MenuAction dialogAction = (new MenuAction()).setOpcode(ActionOpcode.INTERFACE_DIALOG).setPrimary(0).setAction("Continue");
+        InterfaceComponent childOptionsContainer = Interfaces.getComponent(DIALOG_GROUP, 1);
+
         for (String option : options) {
             InterfaceComponent optionComp = Interfaces.getComponent(DIALOG_GROUP, comp ->
                     comp.hasText(option));
+
+            if (option == null && childOptionsContainer != null) { // See if option is grandchild component
+                optionComp = getFirstMatch(childOptionsContainer, options);
+            }
             if (optionComp != null) {
                 new InterfaceInteractEvent(optionComp, dialogAction).setCompleteCondition(Dialog::isProcessing).execute();
                 return true;
@@ -30,15 +42,40 @@ public class ExDialog {
 
     public static boolean process(int index) {
         InterfaceComponent[] optionComps = Interfaces.getComponent(DIALOG_GROUP);
-        if (optionComps.length <= index) {
+        InterfaceComponent childOptionsContainer = Interfaces.getComponent(DIALOG_GROUP, 1);
+        InterfaceComponent targetOptionComp = null;
+
+        if (optionComps.length > index) {
+            targetOptionComp = optionComps[index];
+        }
+        if (targetOptionComp == null) {
+            targetOptionComp = getFirstMatch(childOptionsContainer, index);
+        }
+        if (targetOptionComp == null) {
             return false;
         }
+
         MenuAction dialogAction = (new MenuAction()).setOpcode(ActionOpcode.INTERFACE_DIALOG).setPrimary(0).setAction("Continue");
-        InterfaceComponent targetOptionComp = optionComps[index];
-        if (targetOptionComp != null) {
-            new InterfaceInteractEvent(targetOptionComp, dialogAction).setCompleteCondition(Dialog::isProcessing).execute();
-            return true;
+        new InterfaceInteractEvent(targetOptionComp, dialogAction).setCompleteCondition(Dialog::isProcessing).execute();
+        return true;
+    }
+
+    // Gets first component that contains any option text
+    private static InterfaceComponent getFirstMatch(InterfaceComponent parent, String... options) {
+        for (InterfaceComponent child : parent.getChildren()) {
+            if (Arrays.stream(options).anyMatch(parent::hasText)) {
+                return child;
+            }
         }
-        return false;
+        return null;
+    }
+
+    // Gets first component that contains any option text
+    private static InterfaceComponent getFirstMatch(InterfaceComponent parent, int index) {
+        InterfaceComponent[] children = parent.getChildren();
+        if (children.length > index) {
+            return children[index];
+        }
+        return null;
     }
 }
