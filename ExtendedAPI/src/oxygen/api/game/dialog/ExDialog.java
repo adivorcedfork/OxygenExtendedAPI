d2e38fce04a3ee2eaa93ab52370f09f0bc3b9a54
Jon Smith <ilovebball4life@gmail.com>
2020-11-28 20:54:59 -0600

    Added dialog methods

--- ExtendedAPI/src/oxygen/api/game/dialog/ExDialog.java
+++ ExtendedAPI/src/oxygen/api/game/dialog/ExDialog.java
@@ -1,6 +1,7 @@
 package oxygen.api.game.dialog;
 
 import oxygen.api.accessors.Interfaces;
+import oxygen.api.wrappers.InterfaceComponent;
 
 public class ExDialog {
 
@@ -8,5 +9,28 @@ public class ExDialog {
 
     public static boolean isOpen() {
         return Interfaces.isOpen(DIALOG_GROUP);
+    }
+
+    public static boolean process(String... options) {
+        for (String option : options) {
+            InterfaceComponent optionComp = Interfaces.getComponent(DIALOG_GROUP, comp ->
+                    comp.hasText(option));
+            if (optionComp != null && optionComp.getActions().length > 0) {
+                return optionComp.interact(optionComp.getActions()[0]);
+            }
+        }
+        return false;
+    }
+
+    public static boolean process(int index) {
+        InterfaceComponent[] optionComps = Interfaces.getComponent(DIALOG_GROUP);
+        if (optionComps.length <= index) {
+            return false;
+        }
+        InterfaceComponent targetOptionComp = optionComps[index];
+        if (targetOptionComp != null && targetOptionComp.getActions().length > 0) {
+            return targetOptionComp.interact(targetOptionComp.getActions()[0]);
+        }
+        return false;
     }
 }
